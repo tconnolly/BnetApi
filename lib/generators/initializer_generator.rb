0a667686d61d373163b51fe31d1110f3f031d225
Tom Connolly <tconnolly1991@gmail.com>
2014-08-23 12:26:50 +0100

    created new generator

--- lib/generators/initializer_generator.rb
+++
@@ -1,7 +0,0 @@
-class InitializerGenerator < Rails::Generators::Base
-  source_root File.expand_path("../templates", __FILE__)
-  
-  def copy_initializer_file
-    copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
-  end
-end
