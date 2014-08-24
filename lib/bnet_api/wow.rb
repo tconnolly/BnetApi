a0cbf16084dcf978f8f63223da52e1276bdbee76
Tom Connolly <tconnolly1991@gmail.com>
2014-08-24 01:19:45 +0100

    created spec for wow oauth profile

--- lib/bnet_api/wow.rb
+++ lib/bnet_api/wow.rb
@@ -1,3 +1,4 @@
 require 'bnet_api/wow/achievement/achievement'
 require 'bnet_api/wow/guild/guild'
 require 'bnet_api/wow/character/character'
+require 'bnet_api/wow/oauth/oauth_profile'
--- spec/bnet_api/wow/oauth/oauth_profile_spec.rb
+++ spec/bnet_api/wow/oauth/oauth_profile_spec.rb
@@ -4,15 +4,27 @@ describe BnetApi::WoW::OAuthProfile do
   
   describe "default instance attributes" do
     
-    #let(:career_profile) { BnetApi::D3::CareerProfile.new("Ragwolf#2851") }
+    let(:oauth_profile) { BnetApi::WoW::OAuthProfile.new }
     
-    #
+    it "must have a characters attribute" do
+      oauth_profile.must_respond_to :characters
+    end
+    
+  end
+  
+  describe "default instance methods" do
+    
+    let(:oauth_profile) { BnetApi::WoW::OAuthProfile.new }
+      
+    it "must have a get method" do
+      oauth_profile.must_respond_to :get
+    end
     
   end
   
   describe "GET oauth profile" do
     
-    #let(:career_profile) { BnetApi::D3::CareerProfile.new("Ragwolf#2851") }
+    let(:oauth_profile) { BnetApi::WoW::OAuthProfile.new }
     
     before do
       VCR.insert_cassette "oauth_profile", record: :new_episodes
@@ -22,10 +34,18 @@ describe BnetApi::WoW::OAuthProfile do
       VCR.eject_cassette
     end
     
-    it "records the fixture" do
+    it "must get the right oauth profile" do
       BnetApi.oauth_token = :nfpead9ubmch2f2g5xt4gxxh
-      BnetApi::WoW::OAuthProfile.new.get
+      oauth_profile.get
+      
+      oauth_profile.characters.wont_be_nil
     end
+    
+    #it "records the fixture" do
+    #  BnetApi.oauth_token = :nfpead9ubmch2f2g5xt4gxxh
+    #  o = BnetApi::WoW::OAuthProfile.new
+    #  o.get
+    #end
   end
   
 end
