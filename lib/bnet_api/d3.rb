0f41bc1f24c0c52d24d5e06914581ef5b4602cfe
tconnolly <tconnolly1991@gmail.com>
2014-12-09 15:09:30 +0000

    added return docs to d3 methods

--- lib/bnet_api/d3.rb
+++ lib/bnet_api/d3.rb
@@ -8,6 +8,7 @@ module BnetApi
     # Retrieves the career profile for the user with the specified battletag.
     #
     # @param battletag [String] The battletag of the user profile.
+    # @return [Hash] A hash containing the career profile data.
     def career_profile(battletag)
       BnetApi.make_request("/d3/profile/#{URI.encode(battletag)}/")
     end
@@ -17,6 +18,7 @@ module BnetApi
     #
     # @param battletag [String] The battletag of the user profile.
     # @param id [Integer] The ID of the hero.
+    # @return [Hash] A hash containing the hero profile data.
     def hero_profile(battletag, id)
       BnetApi.make_request("/d3/profile/#{URI.encode(battletag)}/hero/#{id}")
     end
@@ -24,6 +26,7 @@ module BnetApi
     # Retrieves the data for an item with the specified item data string.
     #
     # @param data [String] The item data string of the item.
+    # @return [Hash] A hash containing the item data.
     def item_data(data)
       BnetApi.make_request("/d3/data/item/#{data}")
     end
@@ -31,6 +34,7 @@ module BnetApi
     # Retrieves the data for the specified follower.
     #
     # @param follower [String] The name of the follower.
+    # @return [Hash] A hash containing the follower data.
     def follower_data(follower)
       BnetApi.make_request("/d3/data/follower/#{follower}")
     end
@@ -38,6 +42,7 @@ module BnetApi
     # Retrieves the data for the specified artisan.
     #
     # @param artisan [String] The name of the artisan.
+    # @return [Hash] A hash containing the artisan data.
     def artisan_data(artisan)
       BnetApi.make_request("/d3/data/artisan/#{artisan}")
     end
