492d52a0eb7446acf3dd5c1b6bea9fac08a449c8
tconnolly <tconnolly1991@gmail.com>
2014-12-09 15:10:23 +0000

    added return docs to sc2 methods

--- lib/bnet_api/sc2.rb
+++ lib/bnet_api/sc2.rb
@@ -10,6 +10,7 @@ module BnetApi
     # @param id [Integer] The user's profile ID.
     # @param region [Integer] The region the user is in.
     # @param name [String] The name of the user's profile.
+    # @return [Hash] A hash containing the profile data.
     def profile(id, region, name)
       BnetApi.make_request("/sc2/profile/#{id}/#{region}/#{name}/")
     end
@@ -19,6 +20,7 @@ module BnetApi
     # @param id [Integer] The user's profile ID.
     # @param region [Integer] The region the user is in.
     # @param name [String] The name of the user's profile.
+    # @return [Hash] A hash containing the profile's ladder data.
     def profile_ladders(id, region, name)
       BnetApi.make_request("/sc2/profile/#{id}/#{region}/#{name}/ladders")
     end
@@ -28,6 +30,7 @@ module BnetApi
     # @param id [Integer] The user's profile ID.
     # @param region [Integer] The region the user is in.
     # @param name [String] The name of the user's profile.
+    # @return [Hash] A hash containing the profile's match history data.
     def match_history(id, region, name)
       BnetApi.make_request("/sc2/profile/#{id}/#{region}/#{name}/matches")
     end
@@ -35,16 +38,19 @@ module BnetApi
     # Retrieves the ladder data for the ladder with the specified ID.
     #
     # @param id [String] The ID of the ladder.
+    # @return [Hash] A hash containing the ladder data.
     def ladder(id)
       BnetApi.make_request("/sc2/ladder/#{id}")
     end
 
     # Retrieves all achievements data.
+    # @return [Hash] A hash containing the achievements data.
     def achievements_data
       BnetApi.make_request('/sc2/data/achievements')
     end
 
     # Retrieves all rewards data.
+    # @return [Hash] A hash containing the rewards data.
     def rewards_data
       BnetApi.make_request('/sc2/data/rewards')
     end
