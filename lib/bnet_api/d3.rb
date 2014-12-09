13237290c32a4a59d36337b1001121749fcba9a4
tconnolly <tconnolly1991@gmail.com>
2014-12-09 15:03:56 +0000

    added documentation for D3 and SC2 code

--- lib/bnet_api/d3.rb
+++ lib/bnet_api/d3.rb
@@ -1,25 +1,43 @@
 require 'uri'
 
 module BnetApi
+  # All API methods relating to Diablo III are contained in this module.
   module D3
     extend self
 
+    # Retrieves the career profile for the user with the specified battletag.
+    #
+    # @param battletag [String] The battletag of the user profile.
     def career_profile(battletag)
       BnetApi.make_request("/d3/profile/#{URI.encode(battletag)}/")
     end
 
+    # Retrieves the hero profile for the hero with the specified ID belonging
+    # to the user with the specified battletag.
+    #
+    # @param battletag [String] The battletag of the user profile.
+    # @param id [Integer] The ID of the hero.
     def hero_profile(battletag, id)
       BnetApi.make_request("/d3/profile/#{URI.encode(battletag)}/hero/#{id}")
     end
 
+    # Retrieves the data for an item with the specified item data string.
+    #
+    # @param data [String] The item data string of the item.
     def item_data(data)
       BnetApi.make_request("/d3/data/item/#{data}")
     end
 
+    # Retrieves the data for the specified follower.
+    #
+    # @param follower [String] The name of the follower.
     def follower_data(follower)
       BnetApi.make_request("/d3/data/follower/#{follower}")
     end
 
+    # Retrieves the data for the specified artisan.
+    #
+    # @param artisan [String] The name of the artisan.
     def artisan_data(artisan)
       BnetApi.make_request("/d3/data/artisan/#{artisan}")
     end
--- lib/bnet_api/sc2.rb
+++ lib/bnet_api/sc2.rb
@@ -1,29 +1,50 @@
 require 'uri'
 
 module BnetApi
+  # All API methods relating to Starcraft II are contained in this module.
   module SC2
     extend self
 
+    # Retrieves the profile for specified user.
+    #
+    # @param id [Integer] The user's profile ID.
+    # @param region [Integer] The region the user is in.
+    # @param name [String] The name of the user's profile.
     def profile(id, region, name)
       BnetApi.make_request("/sc2/profile/#{id}/#{region}/#{name}/")
     end
 
+    # Retrieves the ladders a profile is in.
+    #
+    # @param id [Integer] The user's profile ID.
+    # @param region [Integer] The region the user is in.
+    # @param name [String] The name of the user's profile.
     def profile_ladders(id, region, name)
       BnetApi.make_request("/sc2/profile/#{id}/#{region}/#{name}/ladders")
     end
 
+    # Retrieves the match history for the specified profile.
+    #
+    # @param id [Integer] The user's profile ID.
+    # @param region [Integer] The region the user is in.
+    # @param name [String] The name of the user's profile.
     def match_history(id, region, name)
       BnetApi.make_request("/sc2/profile/#{id}/#{region}/#{name}/matches")
     end
 
+    # Retrieves the ladder data for the ladder with the specified ID.
+    #
+    # @param id [String] The ID of the ladder.
     def ladder(id)
       BnetApi.make_request("/sc2/ladder/#{id}")
     end
 
+    # Retrieves all achievements data.
     def achievements_data
       BnetApi.make_request('/sc2/data/achievements')
     end
 
+    # Retrieves all rewards data.
     def rewards_data
       BnetApi.make_request('/sc2/data/rewards')
     end
